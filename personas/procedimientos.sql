cdfc79eea6ac801a8c353f632db6dac6333f8fe2
lvegadi <93616988+lvegadi@users.noreply.github.com>
2022-03-15 22:58:18 -0500

    Agregado el CREATE

--- personas/procedimientos.sql
+++ personas/procedimientos.sql
@@ -1,25 +1,47 @@
+-- Procedimiento para mostrar las notas activas
 CREATE OR REPLACE PROCEDURE sp_listar_notas(notas out SYS_REFCURSOR)
 IS
 BEGIN
-    OPEN notas FOR SELECT N.id, (A.nombres||' '||A.apellidos)as nombre, asg.nombre as asignatura , N.n1, N.n2, N.n3,round(((N.n1 + N.n2)*0.3 + N.n3*0.4),1) as definitiva
+    OPEN notas FOR SELECT N.id,A.id as alumno_id, (A.nombres||' '||A.apellidos)as nombre, asg.nombre as asignatura , N.n1, N.n2, N.n3,round(((N.n1 + N.n2)*0.3 + N.n3*0.4),1) as definitiva
     FROM personas_nota N INNER JOIN personas_alumno A ON A.id = N.alumno_id INNER JOIN personas_asignatura asg ON asg.id = n.asignatura_id 
     WHERE N.ACTIVO = '1';
 END;
 
-CREATE OR REPLACE PROCEDURE sp_listar_notas(notas out SYS_REFCURSOR)
+-- Procedimiento para mostrar los alumnos activados
+CREATE OR REPLACE PROCEDURE sp_listar_alumnos(alumnos out SYS_REFCURSOR)
 IS
 BEGIN
-    OPEN notas FOR SELECT (A.nombres||' '||A.apellidos)as nombre, asg.nombre as asignatura , N.n1, N.n2, N.n3,round(((N.n1 + N.n2)*0.3 + N.n3*0.4),1) as definitiva
-    FROM personas_nota N INNER JOIN personas_alumno A ON A.id = N.alumno_id INNER JOIN personas_asignatura asg ON asg.id = n.asignatura_id 
-    WHERE N.ACTIVO = '1';
+    OPEN alumnos FOR SELECT * FROM personas_alumno WHERE activo = '1';
 END;
+
+-- Procedimiento para mostrar las asignaturas activadas
+CREATE OR REPLACE PROCEDURE sp_listar_asignaturas(asignaturas OUT SYS_REFCURSOR)
+IS
+BEGIN
+    OPEN asignaturas FOR SELECT * FROM personas_asignatura WHERE activo = '1';
+END;
+
+/* procedimiento al. para agregar notas del CRUD de notas
+    salida = 1 --> se inserto la nota
+    salida = 0 --> fallo el insert
+*/
 
 CREATE OR REPLACE PROCEDURE sp_agregar_notas(
     v_alumno_id NUMBER,
     v_asignatura_id NUMBER,
-    v_n1 NUMBER(2,1),
-    v_n2 NUMBER(2,1),
-    v_n3 NUMBER(2,1),
-    v_salida,
- )
+    v_n1 NUMBER,
+    v_n2 NUMBER,
+    v_n3 NUMBER,
+    v_salida OUT NUMBER
+ ) IS
+ BEGIN
+    INSERT INTO personas_nota(alumno_id,asignatura_id,n1, n2, n3,ACTIVO)
+    VALUES (v_alumno_id, v_asignatura_id,v_n1, v_n2, v_n3,1);
+    COMMIT;
+    v_salida := 1;
+
+EXCEPTION
+    WHEN OTHERS THEN
+        v_salida := 0;
+END;
 
--- personas/views.py
+++ personas/views.py
@@ -1,20 +1,64 @@
-import django
+import django, cx_Oracle
 from django.shortcuts import render
 from django.db import connection
 # Create your views here.
 def notas(request):
     data = {
-        'notas':listar_notas()
+        'notas':listar_notas(),
+        'alumnos':listar_alumnos(),
+        'asignaturas':listar_asignaturas()
     }
+    
+    if request.method == 'POST':
+        alumno = request.POST.get('alumno')
+        asignatura = request.POST.get('asignatura')
+        n1 = request.POST.get('n1')        
+        n2 = request.POST.get('n2')
+        n3 = request.POST.get('n3')
+        salida = agregar_nota(alumno, asignatura, n1, n2, n3)
+        if salida == 1:
+            data['mensaje'] = 'agregado correctamente'
+        else:
+             data['mensaje'] = 'No se pudo guardar'
+
     return render(request,'notas/crud.html',data)
 
+
+
+# Metodos para llamar a los cursores
 def listar_notas():
     django_cursor = connection.cursor()
     cursor = django_cursor.connection.cursor() # Cursor que llama
     out_cur = django_cursor.connection.cursor() # cursor que recibe
     cursor.callproc('SP_LISTAR_NOTAS',[out_cur])
-
     lista  = []
     for fila in out_cur:
         lista.append(fila)
     return lista
+
+def listar_alumnos():
+    django_cursor = connection.cursor()
+    cursor = django_cursor.connection.cursor() # Cursor que llama
+    out_cur = django_cursor.connection.cursor() # cursor que recibe
+    cursor.callproc('SP_LISTAR_ALUMNOS',[out_cur])
+    lista  = []
+    for fila in out_cur:
+        lista.append(fila)
+    return lista
+
+def listar_asignaturas():
+    django_cursor = connection.cursor()
+    cursor = django_cursor.connection.cursor() # Cursor que llama
+    out_cur = django_cursor.connection.cursor() # cursor que recibe
+    cursor.callproc('SP_LISTAR_ASIGNATURAS',[out_cur])
+    lista  = []
+    for fila in out_cur:
+        lista.append(fila)
+    return lista
+
+def agregar_nota(alumno, asignatura, n1, n2 ,n3):
+    django_cursor = connection.cursor()
+    cursor = django_cursor.connection.cursor()
+    salida = cursor.var(cx_Oracle.NUMBER)
+    cursor.callproc('SP_AGREGAR_NOTAS',[alumno, asignatura, n1, n2, n3,salida])
+    return salida.getvalue()
