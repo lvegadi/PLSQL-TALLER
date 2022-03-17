755fe580edaa4b0b96310c901b603ff71a583ff0
lvegadi <93616988+lvegadi@users.noreply.github.com>
2022-03-17 13:59:14 -0500

    Vistas Editar/borrar

--- personas/urls.py
+++ personas/urls.py
@@ -3,4 +3,8 @@ from .views import *
 
 urlpatterns = [
     path('',notas,name='notas'),
+    path('edit/<int:id>',notas_editar,name="editar_nota"),
+    path('edit/',notas_editar,name="editar_nota"),
+    path('delete/<int:id>',notas_borrar,name="editar_nota"),
+    path('delete/',notas_borrar,name="editar_nota"),
 ]
--- personas/views.py
+++ personas/views.py
@@ -27,6 +27,42 @@ def notas(request):
     #eliminar_nota(43)
     return render(request,'notas/crud.html',data)
 
+def notas_editar(request,id=None):
+    if id is not None:
+        data = {
+            'alumnos':listar_alumnos(),
+            'id': id,
+            'asignaturas':listar_asignaturas()
+            
+        }
+        if request.method == 'POST':
+            alumno = request.POST.get('alumno')
+            asignatura = request.POST.get('asignatura')
+            year = request.POST.get('year')
+            n1 = request.POST.get('n1')        
+            n2 = request.POST.get('n2')
+            n3 = request.POST.get('n3')
+            salida = editar_nota(id,alumno, asignatura, year, n1, n2, n3)
+            if salida == 1:
+                data['mensaje'] = 'editado correctamente'
+
+            else:
+                data['mensaje'] = 'No se pudo guardar'
+        return render(request,'notas/editar.html',data)
+    else:
+        return notas(request);
+
+def notas_borrar(request,id=None):
+    if id is not None:
+        salida = eliminar_nota(id)
+    return notas(request);
+
+
+def auditorias(request):
+    data = {
+        'auditorias':listar_auditorias(),
+    }
+    return render(request,'notas/auditorias.html',data)
 
 
 # Metodos para llamar a los cursores
@@ -59,6 +95,17 @@ def listar_asignaturas():
     for fila in out_cur:
         lista.append(fila)
     return lista
+
+def listar_auditorias():
+    django_cursor = connection.cursor()
+    cursor = django_cursor.connection.cursor() # Cursor que llama
+    out_cur = django_cursor.connection.cursor() # cursor que recibe
+    cursor.callproc('SP_LISTAR_AUDITORIAS',[out_cur])
+    lista  = []
+    for fila in out_cur:
+        lista.append(fila)
+    return lista
+
 
 def agregar_nota(alumno, asignatura,year, n1, n2 ,n3):
     django_cursor = connection.cursor()
